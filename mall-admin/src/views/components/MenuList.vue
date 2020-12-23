9717e30a643dade588e49d6005712aebccbb0e23
vmax-js <771835525@qq.com>
2020-12-23 21:39:03 +0800

    菜单栏权限配置

--- mall-admin/src/views/components/MenuList.vue
+++ mall-admin/src/views/components/MenuList.vue
@@ -1,30 +1,23 @@
 <template>
   <div class="menu-list">
-    <a-menu
-      mode="inline"
-      theme="dark"
+    <a-menu mode="inline" theme="dark"
       :inline-collapsed="$store.state.collapsed"
-    >
-
+      >
       <a-sub-menu v-for="router in $store.state.menuRoutes"
-       :key="router.name">
-        <span slot="title">
-          <a-icon type="mail" />
-          <span>{{ router.meta.title }}</span>
-        </span>
-        <a-menu-item v-for="child in router.children"
-         :key="child.name">
-         {{ child.meta.title }}
-         </a-menu-item>
-      </a-sub-menu>
-
+        :key="router.name">
+          <span slot="title">
+            <a-icon type="mail" />
+            <span>{{ router.meta.title }}</span>
+          </span>
+          <a-menu-item v-for="child in router.children"
+            :key="child.name">{{ child.meta.title }}</a-menu-item>
+        </a-sub-menu>
     </a-menu>
   </div>
 </template>
 
 <script>
-export default {
-};
+export default {};
 </script>
 
 <style>
